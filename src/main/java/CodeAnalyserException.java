3d6a12adfc597329424c88f327eabf15be7ed672
smrutikapatil <smruti.patil17@gmail.com>
2022-04-09 14:20:56 +0530

    Added- Analyzerto load Indian States Census information from CSV file

--- src/main/java/CodeAnalyserException.java
+++ src/main/java/CodeAnalyserException.java
@@ -1,16 +1,16 @@
-public class CensusAnalyserException extends Exception {
+public class CodeAnalyserException extends Exception {
     enum ExceptionType {
         CENSUS_FILE_PROBLEM, WRONG_FILE_TYPE, INCORRECT_DELIMITER_OR_HEADER
     }
 
     ExceptionType type;
 
-    public CensusAnalyserException(String message, ExceptionType type) {
+    public CodeAnalyserException(String message, ExceptionType type) {
         super(message);
         this.type = type;
     }
 
-    public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
+    public CodeAnalyserException(String message, ExceptionType type, Throwable cause) {
         super(message, cause);
         this.type = type;
     }
